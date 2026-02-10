f178b2e30f4e7696c4aca1123568c57e4ac6c585
luis267 <105466531+luis267@users.noreply.github.com>
2026-02-10 18:28:15 +0000

    tela streaming de filme

--- 2858-java-screenmatch-aula01/src/Main.java
+++ 2858-java-screenmatch-aula01/src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Alô Mundo!");
+        System.out.println("Bem vindo ao placeholderflix");
+        System.out.println("Filme: Kill Bill");
+
+        int anoDeLancamento = 2003;
+        System.out.println("Ano de lançamento : " + anoDeLancamento); 
+        //Concatenação = juntar uma string e uma variavel(neste caso o  anoDeLancamento)
+
+        boolean incluidoNoPlano = true;
+        double notaDeFilme = 8.2;
+
+        double media = (8.2 + 9.0) /2;
+        System.out.println("A média da nota do filme é : " + media);
+
+        String sinopse;
+        sinopse = 
+        """
+        A ex-assassina conhecida apenas como Noiva acorda de um coma de quatro anos decidida a se vingar de Bill, seu ex-amante e chefe, que tentou matá-la no dia do casamento. Ela está motivada a acertar as contas com cada uma das pessoas envolvidas com a perda da filha, da festa de casamento e dos quatro anos de sua vida. Na jornada, a Noiva é submetida a dores físicas agonizantes ao enfrentar a inescrupulosa gangue de Bill, o Esquadrão Assassino de Víboras Mortais.   
+        """;
+        System.out.println(sinopse);
+
     }
 }
