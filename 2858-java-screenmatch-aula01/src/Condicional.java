ca5d28ae05d6fe8f18948711467275e042386a94
luis267 <105466531+luis267@users.noreply.github.com>
2026-02-11 18:39:56 +0000

    Classe condicional funcionando.

--- 2858-java-screenmatch-aula01/src/Condicional.java
+++ 2858-java-screenmatch-aula01/src/Condicional.java
@@ -1,3 +1,22 @@
 public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2003;
+        boolean incluidoNoPlano = true;
+        double notaDeFilme = 8.2;
+        String tipoDoPlano = "premium";
 
+        if (anoDeLancamento > 2025) {
+            System.out.println("O filme é um lançamento recente. ");
+            } else {
+            System.out.println("O filme é um classico. ");
+            
+        }
+
+        if (incluidoNoPlano && tipoDoPlano.equals("premium")) {
+            System.out.println("Disponivel para assistir. ");
+        } else {
+            System.out.println("Filme não disponivel para o seu plano. ");
+        }
+            
+    }
 }
