7706935f3b372ff82c463660de1bb5a84d2a7953
luis267 <105466531+luis267@users.noreply.github.com>
2026-02-19 18:59:15 +0000

    Leitura pronto

--- 2858-java-screenmatch-aula01/src/Leitura.java
+++ 2858-java-screenmatch-aula01/src/Leitura.java
@@ -6,6 +6,17 @@ public class Leitura {
 
         System.out.println("Digite qual o seu filme favorito: ");
         String filme = leitura.nextLine();
+
+        System.out.println("Qual o ano de lançamento do filme? ");
+        int anoDeLancamento = leitura.nextInt();
+        
+        System.out.println("Escreva sua avaliação: ");
+        Double avaliacao = leitura.nextDouble();
+        
+        
         System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliacao);
+
     }
 }
