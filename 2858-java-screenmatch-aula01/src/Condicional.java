d1dc211c874bb7238dafe9092604daf2ba76e4a3
luis267 <105466531+luis267@users.noreply.github.com>
2026-02-19 17:13:40 +0000

    Leitura ainda incompleto

--- 2858-java-screenmatch-aula01/src/Condicional.java
+++ 2858-java-screenmatch-aula01/src/Condicional.java
@@ -7,9 +7,9 @@ public class Condicional {
 
         if (anoDeLancamento > 2025) {
             System.out.println("O filme é um lançamento recente. ");
-            } else {
+        } else {
             System.out.println("O filme é um classico. ");
-            
+
         }
 
         if (incluidoNoPlano && tipoDoPlano.equals("premium")) {
@@ -17,6 +17,59 @@ public class Condicional {
         } else {
             System.out.println("Filme não disponivel para o seu plano. ");
         }
-            
+
+        /*
+         * ALTERNATIVA PARA USO DE IF-ELSE: SWITCH-CASE :
+         * 
+         * switch (expressão) {
+         * case valor1:
+         * // código a ser executado se a expressão for igual a valor1
+         * break;
+         * case valor2:
+         * // código a ser executado se a expressão for igual a valor2
+         * break;
+         * case valor3:
+         * // código a ser executado se a expressão for igual a valor3
+         * break;
+         * ...
+         * default:
+         * // código a ser executado se a expressão não for igual a nenhum valor
+         * break;
+         * }
+         * 
+         */
+
+        int dia = 3;
+        String nomeDia;
+
+        switch (dia) {
+            case 1:
+                nomeDia = "domingo";
+                break;
+            case 2:
+                nomeDia = "segunda-feira";
+                break;
+            case 3:
+                nomeDia = "terça-feira";
+                break;
+            case 4:
+                nomeDia = "quarta-feira";
+                break;
+            case 5:
+                nomeDia = "quinta-feira";
+                break;
+            case 6:
+                nomeDia = "sexta-feira";
+                break;
+            case 7:
+                nomeDia = "sábado";
+                break;
+            default:
+                nomeDia = "Dia inválido";
+                break;
+        }
+        
+
+        System.out.println("O dia " + dia + " é " + nomeDia);
     }
 }
